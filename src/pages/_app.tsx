e1fc671b97f0a37083e4a9b0002cf02748ee98a8
Fiberx01 <75884799+fiberx10@users.noreply.github.com>
2023-08-23 20:35:28 +0100

    fix issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,35 +12,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
-      
+
       <Script id="paypal-script" src={`https://www.paypal.com/sdk/js?client-id=${process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID}&currency=USD&buyer-country=US&merchant-id=${process.env.NEXT_PUBLIC_PAYPAL_MERCHANT_ID}&components=applepay`}>
       </Script>
       <Script id="apple-script" src="https://applepay.cdn-apple.com/jsapi/v1/apple-pay-sdk.js"></Script>
-      <Script strategy="lazyOnload" id="applepay-script">
-        {
-          `
-          
-          if (!window.ApplePaySession) {
-            console.error('This device does not support Apple Pay');
-          }
-         
-          if (!ApplePaySession.canMakePayments()) {
-            console.error('This device is not capable of making Apple Pay payments');
-          }
-        
-          const applepay = paypal.Applepay();
-          applepay.config()
-            .then(applepayConfig => {
-              if (applepayConfig.isEligible) {
-                document.getElementById("applepay-container").innerHTML = '<apple-pay-button id="btn-appl" buttonstyle="black" type="buy" locale="en">';
-              }
-            })
-            .catch(applepayConfigError => {
-              console.error('Error while fetching Apple Pay configuration.');
-            });
-          `
-        }
-      </Script>
+    
     </SessionProvider>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable  @typescript-eslint/restrict-template-expressions */
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
@@ -7,6 +12,34 @@ import Script from "next/script";
 
 export default function Home() {
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  if (!window.ApplePaySession) {
+    console.error('This device does not support Apple Pay');
+  }
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  if (!ApplePaySession.canMakePayments()) {
+    console.error('This device is not capable of making Apple Pay payments');
+  }
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const applepay = paypal.Applepay();
+  applepay.config()
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    .then(applepayConfig => {
+      if (applepayConfig.isEligible) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        document.getElementById("applepay-container").innerHTML = '<apple-pay-button id="btn-appl" buttonstyle="black" type="buy" locale="en">';
+      }
+    })
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    .catch(applepayConfigError => {
+      console.error('Error while fetching Apple Pay configuration.');
+    });
 
   return (
     <>
