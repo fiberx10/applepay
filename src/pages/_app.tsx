7cd1e5ab202af4c23c8b8863c78efe27f93029b9
Fiberx01 <75884799+fiberx10@users.noreply.github.com>
2023-08-23 20:09:32 +0100

    base code

---
+++ src/pages/_app.tsx
@@ -0,0 +1,48 @@
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
+import { api } from "@/utils/api";
+import "@/styles/globals.css";
+import Script from "next/script";
+import { env } from "@/env.mjs";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+      <Script id="paypal-script" src={`https://www.paypal.com/sdk/js?client-id=${env.NEXT_PUBLIC_PAYPAL_CLIENT_ID}&currency=USD&buyer-country=US&merchant-id=${env.NEXT_PUBLIC_PAYPAL_MERCHANT_ID}&components=applepay`}>
+      </Script>
+      <Script id="apple-script" src="https://applepay.cdn-apple.com/jsapi/v1/apple-pay-sdk.js"></Script>
+      <Script strategy="lazyOnload" id="applepay-script">
+        {
+          `
+          
+          if (!window.ApplePaySession) {
+            console.error('This device does not support Apple Pay');
+          }
+         
+          if (!ApplePaySession.canMakePayments()) {
+            console.error('This device is not capable of making Apple Pay payments');
+          }
+        
+          const applepay = paypal.Applepay();
+          applepay.config()
+            .then(applepayConfig => {
+              if (applepayConfig.isEligible) {
+                document.getElementById("applepay-container").innerHTML = '<apple-pay-button id="btn-appl" buttonstyle="black" type="buy" locale="en">';
+              }
+            })
+            .catch(applepayConfigError => {
+              console.error('Error while fetching Apple Pay configuration.');
+            });
+          `
+        }
+      </Script>
+    </SessionProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
